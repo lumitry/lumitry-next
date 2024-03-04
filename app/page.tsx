9292e1e161a4445a5995975688d49a1adbd72ecd
Briggs Tucker <53492381+lumitry@users.noreply.github.com>
2024-03-04 12:45:36 -0500

    add projects section (WIP)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { TechnologiesScroller } from "./cards";
+import Projects from "./projects";
 
 export default function Home() {
     return (
         <main>
             {/* <p>Hi!</p> */}
-            <TechnologiesScroller></TechnologiesScroller>
-            {/* TODO: replace these with technologies (angular, primeng, html, css, svelte, scss, javafx, etc.; take from linkedin) */}
+            <TechnologiesScroller />
+            <Projects />
+            {/* TODO: projects component */}
         </main>
     );
 }
---
+++ app/projects.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export default function Projects() {
+    return (
+        <div>
+            <h2 className="mt-4 scroll-m-20 pb-2 text-center text-3xl font-bold tracking-tight text-slate-100">
+                Projects
+            </h2>
+        </div>
+    );
+}
