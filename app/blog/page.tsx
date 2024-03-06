28fbedc8d9626115067a435d87924719120c2a05
Briggs Tucker <53492381+lumitry@users.noreply.github.com>
2024-03-05 21:25:13 -0500

    fix issue with WIP blog route

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,1 +1,17 @@
 // TODO: make blog page and link to it in navbar etc.
+
+import React from "react";
+
+export default function Page() {
+    return (
+        <div className="h-[50vh]">
+            <h2
+                className="mt-8 scroll-m-20 pb-2 text-center text-3xl font-bold tracking-tight text-slate-100"
+                id="Blog"
+            >
+                Blog
+            </h2>
+            <p className="text-center">WIP; Coming soon!</p>
+        </div>
+    );
+}
